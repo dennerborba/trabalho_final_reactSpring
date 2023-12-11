1936170f6a23c48d36d14413b899e6fc7f9f967e
b0rb44 <dennerdeborba@gmail.com>
2023-12-11 20:29:32 -0300

    add react e springJPA

---
+++ SpringJPA/aula_final/src/main/java/com/example/aula_final/AulaFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.aula_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaFinalApplication.class, args);
+	}
+
+}
---
+++ SpringJPA/aula_final/src/test/java/com/example/aula_final/AulaFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aula_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
